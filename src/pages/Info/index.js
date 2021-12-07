4ccfd3e432cbe07794d78c30a0280bfdbafb6b5f
Gustavo Borges <79589163+gBorges19@users.noreply.github.com>
2021-12-07 12:33:14 -0300

    Update index.js

--- src/pages/Info/index.js
+++ src/pages/Info/index.js
@@ -11,14 +11,14 @@ function Info() {
      <ContainerContent>
                <SvgInfo xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-presentation" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="3" y1="4" x2="21" y2="4" /><path d="M4 4v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-10" /><line x1="12" y1="16" x2="12" y2="20" /><line x1="9" y1="20" x2="15" y2="20" /><path d="M8 12l3 -3l2 2l3 -3" /></SvgInfo>
                 <TituloContent>Objetivo</TituloContent>
-                <InfoText>Facilitar a aquisição de dados de hidrotelemetria e as estações existentes cadastradas e gerenciadas pela Agencia Nacional de Aguas (ANA)</InfoText>
+                <InfoText></InfoText>
                 <SvgInfo xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-settings" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
                 <circle cx="12" cy="12" r="3" />
                 </SvgInfo>
                 <TituloContent>Como funciona</TituloContent>
-                <InfoText>funciona assim</InfoText>
+                <InfoText></InfoText>
                 <SvgInfo xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-help" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <circle cx="12" cy="12" r="9" />
@@ -26,14 +26,14 @@ function Info() {
                 <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
                 </SvgInfo>
                 <TituloContent>Como usar</TituloContent>
-                <InfoText>acessar o site</InfoText>
+                <InfoText></InfoText>
                 <SvgInfo xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
                 </SvgInfo>
                 <TituloContent>Github</TituloContent>
-                <InfoText>front-end</InfoText>
-                <InfoText>back-end</InfoText>
+                <InfoText></InfoText>
+                <InfoText></InfoText>
                 <SvgInfo xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-code" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <polyline points="7 8 3 12 7 16" />
